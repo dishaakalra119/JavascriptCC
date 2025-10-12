ab5625c9d47190694e16fa09fc4defd0caea5a12
dishaakalra119 <dishaakalra@gmail.com>
2025-10-12 23:58:35 +0530

    initialisingrepository

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("Dishaa");
+// difference between .js and .txt: there isn't much difference. both can be treated as source code. Both can have some text but the text inside .js file is not just randome text here console menas somethings special or unique.
+// it's structure of syntax is unique to javascript. each programming language comes with its own syntax. right now there isn't much diffrence between .js and .txt file where oth contain the line console.log("Dishaa").
+// Then what makes .js and .txt file different? the differnece is that we can inject a .js file in a software to execute it. like compilers or interpretter
+// these softwares have the capability to understand the syntax or structure. the software will see if the File ends with .js extension and only then accepts ImageTrack. it can then interpret the meaning of the syntax and commands to offer some output or processing based on the syntax of commands ie the software will interpret console.log("Dishaa") and logs that onto the console.
+// for years this execution software was hidden inside the browser and we could only access it through creating an html file and linking our scripts to te html file in order to run them in the browser.
+// if we wanted to make it available seperately we had to configure it and set it up which not everyone could do.
+// then this software was taken from browser and made available standalone, and can now be used in backend, mobiles etc.
+// this runtime environment for js is node js, but others like deno are also available. node is just modt widely used.
